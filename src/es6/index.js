5f65bb978da8a15352ba5c695a87f784322f746b
Carlos Quesada <cquesadad90@gmail.com>
2022-06-25 19:37:34 +0100

    Concatenaciones, multilínea, variables let y con y agrupar arrays

--- src/es6/index.js
+++ src/es6/index.js
@@ -29,3 +29,62 @@ let epicPhrase2 = `${hello} ${world} `
 console.log(epicPhrase2);
 
 
+// Multilínea
+
+let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
++ "otra frase epica que necesitamos."
+
+// es6
+
+let lorem2 = `otra frase epica que necesitamos 
+la segunda frase
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+
+//Extraer elementos de objetos
+let person = {
+    'name': 'Carlos',
+    'age': 31,
+    'country': 'CU' 
+}
+
+console.log(person.name, person.age);
+
+//es6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+//Desestructuración 
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+//Let, variables para guardar en memoria, existe dentro de un bloque solamente
+
+var hola = "Hola";
+
+//Bloque Var
+{
+    var globalVar = "Global Var";
+}
+
+//Bloque Let
+{
+    let globalLet = "Global Let";
+    //Solo se puede llamar dentro del bloque
+    console.log(globalLet);
+}
+
+//Se puede llamar globalmente
+console.log(globalVar);
+
+//const no varía su valor, es constante siempre
+const a = 'b';
+a = 'a';
+console.log(a);
