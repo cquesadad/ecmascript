f8c25d6272e3fcd0c86aa219a2ef401c082e0182
Carlos Quesada <cquesadad90@gmail.com>
2022-06-25 19:15:50 +0100

    Funciones con parámetros y concatenaciones

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,5 @@
+//Default params funciones
+
 function newFunction(name, age, country) {
     var name = name || 'oscar';
     var age = age || 32;
@@ -6,11 +8,24 @@ function newFunction(name, age, country) {
 }
 
 //es6
-
+ 
 function newFunction2(name = 'Oscar', age = 32, country = "ES") {
     console.log(name, age, country);
 }
 
 newFunction2();
 newFunction2('Ricardo', 34, "MX");
+
+//Concatenaciones 
+
+let hello = "Hello";
+let world = "World";
+
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+//es6
+let epicPhrase2 = `${hello} ${world} `
+console.log(epicPhrase2);
+
 
